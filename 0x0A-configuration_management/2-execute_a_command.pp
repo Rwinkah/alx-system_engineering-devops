b8ab819881639d32a830c11da65b2ace61017a1a
Rwinkah <rwinosa@gmail.com>
2023-03-29 02:39:51 +0100

    2 task 0x0A

---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,7 @@
+# Manifest to kill a running task
+exec { 'killing a process using pkill':
+    command  => 'pkill -9 killmenow',
+    path     => '/usr/bin:/bin',
+    onlyif   => 'pgrep killmenow'
+    provider => shell,
+}
