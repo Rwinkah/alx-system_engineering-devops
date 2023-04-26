bdf37f9d36aedfefd0551a89ff2bede52d29dc03
Rwinkah <rwinosa@gmail.com>
2023-04-26 22:59:32 +0100

    fix 0 task

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""Python script using rest API to get a response."""
+"""
+Python script using rest API to get a response.
+"""
 import sys
 import requests as req
 
@@ -9,13 +11,13 @@ def main():
 
     list_url = 'https://jsonplaceholder.typicode.com/'
     todo = req.get(list_url + 'todos', params={'userId': sys.argv[1]}).json()
-    userid = r.get(list_url + 'users/{}'.format(sys.argv[1]}).json()
+    userid = r.get(list_url + 'users/{}'.format(sys.argv[1])).json()
     doneTask = [title.get("title") for title in todo if title.get('completed') is True]
 
-    print(done task)
+    print("done task")
     print("Employee {} is done with tasks({}/{}):".format(userid.get("name"), len(doneTask), len(todo)))
 
-    [print"\t {}".format(title)) for title in donetask]
+    [print ("\t {}".format(title) for title in donetask)]
 
 
 if __name__ == '__main__':
