c04cb23d9e9854cb2fb8d9613833fd9bdf3787ef
Rwinkah <rwinosa@gmail.com>
2023-04-26 23:03:32 +0100

    fix

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -12,12 +12,14 @@ def main():
     list_url = 'https://jsonplaceholder.typicode.com/'
     todo = req.get(list_url + 'todos', params={'userId': sys.argv[1]}).json()
     userid = r.get(list_url + 'users/{}'.format(sys.argv[1])).json()
-    doneTask = [title.get("title") for title in todo if title.get('completed') is True]
+    doneTask = [title.get("title") for
+                title in todo if title.get('completed') is True]
 
     print("done task")
-    print("Employee {} is done with tasks({}/{}):".format(userid.get("name"), len(doneTask), len(todo)))
+    print("Employee {} is done with tasks({}/{}):"
+            .format(userid.get("name"), len(doneTask), len(todo)))
 
-    [print ("\t {}".format(title) for title in donetask)]
+    [print("\t {}".format(title) for title in donetask)]
 
 
 if __name__ == '__main__':
