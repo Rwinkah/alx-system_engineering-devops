e87b5f88601f6447049cf47f0d1ed4ca119c4a04
Rwinkah <rwinosa@gmail.com>
2023-04-27 01:11:28 +0100

    fix

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -15,18 +15,19 @@ def main():
     comp_title = []
     base_url = 'https://jsonplaceholder.typicode.com/'
     todo = json.loads(request.urlopen(base_url + 'todos').read().decode())
-    user = json.loads(request.urlopen(base_url + 'users/' + argv[1]).read().decode())
+    user = json.loads(request.urlopen(base_url + 'users/' + argv[1])
+                      .read().decode())
 
     for i in todo:
         if i['userId'] == int(argv[1]) and i['completed']:
             count += 1
             comp_title.append(i['title'])
-            
 
-    print('Employee {} is done with tasks({}/20 tasks)'.format(user['name'], count))
+    print('Employee {} is done with tasks({}/20 tasks)'
+          .format(user['name'], count))
     for i in comp_title:
         print('     {}'.format(i))
 
+
 if __name__ == '__main__':
     main()
-
---
+++ 0x15-api/tester.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+import urllib.request
+request_url = urllib.request.urlopen('https://www.geeksforgeeks.org/')
+print(request_url.read())
