f64e8db152fe9cb5aee449a17bbb1a4c758b14e1
rwinkah <rwinosa@gmail.com>
2023-07-14 09:43:56 +0100

    fix

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -13,13 +13,14 @@ exec { 'index_page':
   provider => shell,
 }
 
+exec {'redirect':
+	command => sed -i 'i 47i \\tlocation /redirect_me {\n\t\treturn 301 http://www.google.com;\n\t}',
+	provider => shell,
+}
+
 exec { 'start_server':
   command => 'sudo service nginx restart',
   provider => shell,
 }
 
-exec {'redirect':
-	command => sed -i 'i 47i \\tlocation /redirect_me {\n\t\treturn 301 http://www.google.com;\n\t}',
-	provider => shell,
-}
 
